4ad27af7cef8fa0172ee02bb4ac9c3a5e36ca7eb
wahyusyaban <48669571+wahyusyaban@users.noreply.github.com>
2025-05-14 23:59:31 +0800

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,9 +2,9 @@ import type { Config } from './types';
 
 export default {
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'News One Today',
+  shortName: 'News One',
+  description: 'Stay informed with News One – your trusted source. Get real-time updates, in-depth analysis, and accurate reporting to keep you ahead. Visit News One for today!',
   direction: 'auto',
   language: 'en-US',
   backgroundColor: '#fff',
@@ -17,16 +17,16 @@ export default {
   preferRelatedApplications: false,
   shortcuts: [
     {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
+      name: 'Games',
+      shortName: 'Games',
       description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
+      url: '/search/label/Games?utm_source=homescreen',
     },
     {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
+      name: 'Technology',
+      shortName: 'Technology',
       description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      url: '/search/label/Technology?utm_source=homescreen',
     },
   ],
   pwa: {
@@ -39,5 +39,5 @@ export default {
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'http://www.newsonetoday.com/',
 } satisfies Config;
