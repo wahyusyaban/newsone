6554fcb51d2d19aa78cbfc300f624f305db932e6
wahyusyaban <48669571+wahyusyaban@users.noreply.github.com>
2025-05-15 00:15:28 +0800

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -34,7 +34,7 @@ export default {
     // OneSignal is not available if you are not using cloudflare workers
     oneSignalEnabled: false,
     oneSignalConfig: {
-      appId: '********-****-****-****-************',
+      appId: 'de800c7d-61a9-4068-9e1c-561d7ccfccb6',
       allowLocalhostAsSecureOrigin: true,
     },
   },
